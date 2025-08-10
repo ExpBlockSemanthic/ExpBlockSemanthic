293712dd43f224f70f9b6de3a408b8e476f88a92
Daniel Duarte <danielduartefilho.df@gmail.com>
2025-08-10 10:29:39 -0300

    ajustado código para gerar os resultados

--- src/main/java/gr/edu/ihu/expblock/ExpBlock.java
+++ src/main/java/gr/edu/ihu/expblock/ExpBlock.java
@@ -170,8 +170,8 @@ public class ExpBlock {
         long startTime = System.currentTimeMillis();
         long startTimeCycle = System.currentTimeMillis();
         try {
-            CSVReader readerA = new CSVReader(new FileReader("c:\\data\\test_voters_A.txt"));
-            CSVReader readerB = new CSVReader(new FileReader("c:\\data\\test_voters_B.txt"));
+            CSVReader readerA = new CSVReader(new FileReader("C:\\Users\\jose.filho.ext\\Documents\\ExpBlockSemanthic\\target\\test_voters_A.txt"));
+            CSVReader readerB = new CSVReader(new FileReader("C:\\Users\\jose.filho.ext\\Documents\\ExpBlockSemanthic\\target\\test_voters_B.txt"));
 
             String[] lineInArray1;
             String[] lineInArray2;
